3b418c130fc0059dc30fa589fed094083fc09d80
Nolan Brady <nolanrbrady@gmail.com>
2018-09-20 20:45:20 -0600

    Fixing Recursive issue

--- models/user.go
+++ models/user.go
@@ -12,16 +12,16 @@ import (
 
 // User Struct declares the model strucutre for Users
 type User struct {
-	ID            uuid.UUID   `json:"id" db:"id"`
-	CreatedAt     time.Time   `json:"created_at" db:"created_at"`
-	UpdatedAt     time.Time   `json:"updated_at" db:"updated_at"`
-	Name          string      `json:"name" db:"name"`
-	Email         string      `json:"email" db:"email"`
-	Password      string      `json:"password" db:"password"`
-	WalletAddress string      `json:"wallet_address" db:"wallet_address"`
-	Coupon        Coupon      `has_many:"coupons"`
-	Transaction   Transaction `has_many:"transactions"`
-	Inventory     Inventory   `has_many:"inventories"`
+	ID            uuid.UUID    `json:"id" db:"id"`
+	CreatedAt     time.Time    `json:"created_at" db:"created_at"`
+	UpdatedAt     time.Time    `json:"updated_at" db:"updated_at"`
+	Name          string       `json:"name" db:"name"`
+	Email         string       `json:"email" db:"email"`
+	Password      string       `json:"password" db:"password"`
+	WalletAddress string       `json:"wallet_address" db:"wallet_address"`
+	Coupon        *Coupon      `has_many:"coupons"`
+	Transaction   *Transaction `has_many:"transactions"`
+	Inventory     *Inventory   `has_many:"inventories"`
 }
 
 // String is not required by pop and may be deleted
