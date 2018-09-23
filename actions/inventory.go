20e2f22eb41673f92a163ac1095beb2ae7f1e4a4
Nolan Brady <nolanrbrady@gmail.com>
2018-09-23 17:08:45 -0600

    Pull back all items in inevntory

--- actions/inventory.go
+++ actions/inventory.go
@@ -4,8 +4,22 @@ import (
 	"github.com/gobuffalo/buffalo"
 )
 
+//TransactionResource is a support
+type TransactionResource struct{}
+
 // GetInventory is a default handler to serve up
 // a home page.
 func GetInventory(c buffalo.Context) error {
-	return c.Render(200, r.JSON(map[string]string{"message": "Welcome to Get Inventory!"}))
+	db, err := pop.Context("development")
+	if err != nil {
+		fmt.Println("Issue connection to DB", err)
+	}
+	//Connect to models
+	inventory := models.Inventory{}
+	// Find all items in inventory
+	findErr := db.All(&inventory)
+	if findErr != nil {
+		fmt.Println("Issue with find items", findErr)
+	}
+	return c.Render(200, r.JSON(200, r.JSON(inventory))
 }
